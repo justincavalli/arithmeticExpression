0604294e9d7534da257fbe6adfb600d95c64a3f7
Justin <jcavalli@sandiego.edu>
2022-09-26 13:17:51 -0700

    reformat without the need for a second class

--- ExpressionEval.py
+++ ExpressionEval.py
@@ -1,41 +1,43 @@
-class ExpressionEval:
+import sys
 
+class listStack():
     def __init__(self):
-        # implement later
+        # a python list stores the data in the stack
+        self._stack = []
 
-    class listStack():
+    def push(self, x):
+        # pushes x in the stack
+        self._stack.append(x)
 
-        def __init__(self):
-            # a python list stores the data in the stack
-            self._stack = []
+    def pop(self):
+        # pops i.e. removes the last element from the stack and returns it
 
-        def push(self, x):
-            # pushes x in the stack
-            self._stack.append(x)
-
-        def pop(self):
-            # pops i.e. removes the last element from the stack and returns it
-
-            if(self.__is_empty__()):
-                # error if the stack is empty
-                raise Exception('the stack is empty')
-            else:
-                return self._stack.pop()
+        if(self.__is_empty__()):
+            # error if the stack is empty
+            raise Exception('the stack is empty')
+        else:
+            return self._stack.pop()
 
 
-        def peep(self):
-            # returns the latest element from the stack without removing it from the list
+    def peep(self):
+        # returns the latest element from the stack without removing it from the list
 
-            if(self.__is_empty__()):
-                # error if the stack is empty
-                raise Exception('the stack is empty')
-            else:
-                return self._stack[self.size()-1]
+        if(self.__is_empty__()):
+            # error if the stack is empty
+            raise Exception('the stack is empty')
+        else:
+            return self._stack[self.size()-1]
 
-        def size(self):
-            # returns the size of the stack
-            return len(self._stack)
+    def size(self):
+        # returns the size of the stack
+        return len(self._stack)
 
-        def __is_empty__(self):
-            # returns true if the stack is empty
-            return len(self._stack) == 0
+    def __is_empty__(self):
+        # returns true if the stack is empty
+        return len(self._stack) == 0
+         
+def main():
+    print(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
